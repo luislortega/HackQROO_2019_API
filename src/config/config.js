d2502fa3dc21b09867034bc6975f93c25506643c
luislortega <a18016323@alumnos.uady.mx>
2019-12-07 20:05:23 -0500

    Update cloud database

--- src/config/config.js
+++ src/config/config.js
@@ -4,16 +4,16 @@ const op = Sequelize.Op;
 
 module.exports = {
   db: {
-    database: process.env.DB_NAME || 'qdata',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASS || '1298Luis',
+    database: process.env.DB_NAME || 'dumrepjl9u7p',
+    user: process.env.DB_USER || 'uboourixzoiqoy',
+    password: process.env.DB_PASS || '1dca2813c8ad4b7dd211f651c5d45518d04583f4f7b112c0c2798ce515577cff',
     options: {
       dialect: process.env.DIALECT || 'postgres',
-      /*ssl: true,
+      ssl: true,
       dialectOptions: {
         ssl: true
-      },*/
-      host: process.env.HOST || 'localhost',
+      },
+      host: process.env.HOST || 'ec2-50-19-95-77.compute-1.amazonaws.com',
       operatorsAliases: op.gt, //Fix the deprecated string based operator
     },
   },
