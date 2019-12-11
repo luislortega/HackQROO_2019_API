5edada765b6d88b4ad2df2f2153afd76a4133e0a
Luis Gerardo Leon Ortega <leon.luisgerardo775@gmail.com>
2019-12-10 18:31:57 -0600

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -4,16 +4,16 @@ const op = Sequelize.Op;
 
 module.exports = {
   db: {
-    database: process.env.DB_NAME || 'dumrepjl9u7p',
-    user: process.env.DB_USER || 'uboourixzoiqoy',
-    password: process.env.DB_PASS || '1dca2813c8ad4b7dd211f651c5d45518d04583f4f7b112c0c2798ce515577cff',
+    database: process.env.DB_NAME || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASS || '',
     options: {
       dialect: process.env.DIALECT || 'postgres',
       ssl: true,
       dialectOptions: {
         ssl: true
       },
-      host: process.env.HOST || 'ec2-50-19-95-77.compute-1.amazonaws.com',
+      host: process.env.HOST || '',
       operatorsAliases: op.gt, //Fix the deprecated string based operator
     },
   },
